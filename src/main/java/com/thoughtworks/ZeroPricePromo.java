4f594832bca2b016c64a1d87064d0757bbe9735c
RicoTech666 <zhoujiachenbe@gmail.com>
2020-02-16 18:01:20 +0800

    [Jiachen] feat: create zero promo case

--- src/main/java/com/thoughtworks/ZeroPricePromo.java
+++ src/main/java/com/thoughtworks/ZeroPricePromo.java
@@ -1,4 +1,30 @@
 package com.thoughtworks;
 
-public class ZeroPricePromo {
+import com.thoughtworks.interfaces.Promotions;
+
+public class ZeroPricePromo implements Promotions {
+    private OrderInfo orderInfo;
+    private final int fixedPromoPrice = 6;
+    private final int promoThreshold = 30;
+
+    public ZeroPricePromo(OrderInfo orderInfo) {
+        this.orderInfo = orderInfo;
+    }
+
+    @Override
+    public boolean isCurrentPromoType() {
+        int originalTotalPrice = orderInfo.getTotalPrice();
+        int halfPromoAmount = orderInfo.getHalfPromoAmount();
+        return (halfPromoAmount == 0 && originalTotalPrice < promoThreshold);
+    }
+
+    @Override
+    public int getPromotionPrice() {
+        return 0;
+    }
+
+    @Override
+    public String getPromotionResult() {
+        return null;
+    }
 }
